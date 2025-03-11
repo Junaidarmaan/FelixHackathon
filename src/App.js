1a435e11b2e7c3d262885dd46685567e071965bc
junaid <junnubest@gmail.com>
2025-03-11 15:46:26 +0530

    added Objectives section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './Components/Nav';
 import Crousel from './Components/Crousel'
+import Objective from './Components/Objectives';
 
 function App() {
   return (
     <>
     <Nav/>
     <Crousel/>
+    <Objective/>
     </>
   );
 }
---
+++ src/Components/Objectives.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+export default function Objective() {
+    return (
+
+        <>
+            <div class="card text-center">
+                <div class="card-header">
+                    Objectives
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Our Aim And Ojectives IS :</h5>
+                    <p class="card-text">1. To provide remote-access to simulation-based Labs in various disciplines of Science and Engineering..</p>
+                    <p class="card-text">2.To enthuse students to conduct experiments by arousing their curiosity. This would help them in learning basic and advanced concepts through remote experimentation.</p>
+                    <p class="card-text">3.To provide a complete Learning Management System around the Virtual Labs where the students/ teachers can avail the various tools for learning, including additional web-resources, video-lectures, animated demonstrations and self-evaluation..</p>
+                
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                <div class="card-footer text-body-secondary">
+                    2 days ago
+                </div>
+            </div>
+        </>
+    )
+
+
+}
